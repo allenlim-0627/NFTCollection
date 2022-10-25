cce49da87d4b48747bdd5296261ad8b86d802dec
AP\KLIM2 <klim2@steelcase.com>
2022-10-25 20:43:34 +0800

    commit for changes for deployment to opensea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x2628fB1538CD37a84dF71FE242eA143d73BE7a9c";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nftcollection-nine.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
